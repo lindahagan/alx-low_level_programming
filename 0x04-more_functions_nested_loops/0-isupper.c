7ad4af5a1df4611e27422b705bfb29b6bb3d5e14
Linda Hagan <iindahagandooll321@gmail.com>
2023-02-23 12:16:52 -0800

    projects

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+* _isupper - checks for uppercase character
+* @x: Variable tex "x"
+* Return: Always 0.
+*/
+int _isupper(int x)
+{
+        if (x >= 65 && x <= 90)
+        {
+                return (1);
+        }
+        return (0);
+}
---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,11 @@
+#include "main.h"
+/**
+ * mul - Multiple two integers
+ * @x: first nmber to multiply
+ * @y: seconf number to multiply
+ * Return: Always 0.
+*/
+int mul(int x, int y)
+{
+        return (x * y);
+}
